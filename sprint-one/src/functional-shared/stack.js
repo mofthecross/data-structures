6461315d9b32631f2d5fc7654d74a1f2a36223a1
mofthecross <mofthecross@gmail.com>
2016-06-08 15:41:24 -0700

    Implement push and pop correctly

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,12 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  return _.extend(stackMethods);
+  var newStack = _.extend(stackMethods);
+  newStack.storage = {};
+  newStack.stackSize = 0;
+  return newStack;
 };
 
 var stackMethods = {
   size: function() {
-    return 0;
+    return this.stackSize;
+  },
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+  pop: function() {
+    if (this.stackSize !== 0) {
+      this.stackSize--;
+      var out = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      return out;
+    }
   }
 };
 
