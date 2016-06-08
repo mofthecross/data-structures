e8c5bccf8fbbfbe6ad040f8b4b35122d3e26c90a
mofthecross <mofthecross@gmail.com>
2016-06-08 15:55:53 -0700

    Implement functional-shared instantiation style and size method.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  _.extend(newQueue, queueMethods);
+  newQueue.storage = {};
+  newQueue.queueSize = 0;
+  newQueue.front = 0;
+  newQueue.back = 0;
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.queueSize;
+  }
+};
 
 
