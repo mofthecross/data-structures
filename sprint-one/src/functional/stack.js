f80fe60377b92280f5b80a10f86116fbf811699b
mofthecross <mofthecross@gmail.com>
2016-06-08 14:54:29 -0700

    Updates size of stack after pushing items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return count;
   };
 
   return someInstance;
