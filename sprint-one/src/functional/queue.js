fff360e09d4ddb47b6068cf5a9bc0927b7d6deb1
mofthecross <mofthecross@gmail.com>
2016-06-08 15:05:46 -0700

    Updates size when enqueuing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,20 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return size;
   };
 
   return someInstance;
