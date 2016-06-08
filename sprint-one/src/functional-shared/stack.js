2af3e3b9595be0e58b3596a810969185dd4a5507
mofthecross <mofthecross@gmail.com>
2016-06-08 15:29:03 -0700

    Set up functional shared patterns and reports size of 0 for a new stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,13 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return _.extend(stackMethods);
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return 0;
+  }
+};
 
 
