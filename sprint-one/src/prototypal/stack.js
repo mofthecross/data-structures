abceb2a628e2d35dcec43bafe3969d78593b412d
mofthecross <mofthecross@gmail.com>
2016-06-08 19:39:44 -0700

    Implemented Prototypal Instantiation class structure

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,11 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.stackSize = 0;
+  newStack.out; 
+  return newStack;
 };
 
 var stackMethods = {};
-
-
