8987445ce93a146d7c003557c1ce9d9f7e1d439d
mofthecross <mofthecross@gmail.com>
2016-06-09 11:37:45 -0700

    Add values to a Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,20 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] !== undefined) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
