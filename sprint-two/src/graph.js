ff69aca68ad9da7164287ca7540e7ac0021282a7
mofthecross <mofthecross@gmail.com>
2016-06-09 11:10:04 -0700

    Create edges between two nodes on Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,7 +7,7 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.storage[node] = [];
+  this.storage[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -28,10 +28,16 @@ Graph.prototype.removeNode = function(target) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.storage[fromNode][toNode] === this.storage[toNode]) {
+    return true;
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode][toNode] = this.storage[toNode];
+  this.storage[toNode][fromNode] = this.storage[fromNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
