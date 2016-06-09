35184bf16ea5b332362b7115f6ae9069074fda32
mofthecross <mofthecross@gmail.com>
2016-06-08 20:10:10 -0700

    Implement pseudo-clasical pattern and size() method.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,13 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.size = function() {
+  return this.stackSize;
+};
 
+var myStack = new Stack();
