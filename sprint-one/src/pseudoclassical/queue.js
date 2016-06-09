143c8c1b658ddf6e7ec44c98ba48f1c79f8579ec
mofthecross <mofthecross@gmail.com>
2016-06-08 20:19:48 -0700

    Implemented pseudoclassical pattern for Queue().

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,11 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.front = 0;
+  this.queueSize = 0;
+  this.back = 0;
+  this.storage = {};
 };
 
 
+var ourQueue = new Queue();
