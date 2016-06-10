f4108ab4141967efddbf0c0fa681ed9d65aefc57
mofthecross <mofthecross@gmail.com>
2016-06-09 19:04:36 -0700

    Insert values into correct locations in binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,38 @@
 var BinarySearchTree = function(value) {
-};
+  var obj = {};
 
+  obj.value = value;
+  obj.left = undefined;
+  obj.right = undefined;
+
+  obj.insert = function(val) {
+    if (val < this.value) {
+      if (this.left === undefined) {
+        this.left = BinarySearchTree(val); 
+      } else {
+        this.left.insert(val);
+      }
+    } else if (val > this.value) {
+      if (this.right === undefined) {
+        this.right = BinarySearchTree(val);
+      } else {
+        this.right.insert(val);
+      }
+    }
+    
+
+  };
+
+  obj.contains = function(target) {
+
+  };
+
+  obj.depthFirstLog = function(cb) {
+
+  };
+
+  return obj;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
