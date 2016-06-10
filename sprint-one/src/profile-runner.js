5200f2c768494434c31ef696cca6fbdb368b57d0
mofthecross <mofthecross@gmail.com>
2016-06-10 11:31:19 -0700

    Organize script runner for sequential testing

--- sprint-one/src/profile-runner.js
+++ sprint-one/src/profile-runner.js
@@ -1,25 +1,25 @@
   var funcStackArr = [];
   var funcQueueArr = [];
-  var funcShareStackArr = [];
-  var funcShareQueueArr = [];
-  var prototypalStackArr = [];
-  var prototypalQueueArr = [];
-  var pseudoclassicStackArr = [];
-  var pseudoclassicQueueArr = [];
+  // var funcShareStackArr = [];
+  // var funcShareQueueArr = [];
+  // var prototypalStackArr = [];
+  // var prototypalQueueArr = [];
+  // var pseudoclassicStackArr = [];
+  // var pseudoclassicQueueArr = [];
 
 $(document).ready(function() {
 
-  var stackAndQueueSizes = 1000;
+  var stackAndQueueSizes = 500000;
 
   for (var i = 0; i <= stackAndQueueSizes; i++) {
     funcStackArr.push(FunctionalStack());
     funcQueueArr.push(FunctionalQueue());
-    funcShareStackArr.push(FunctionalSharedStack());
-    funcShareQueueArr.push(FunctionalSharedQueue());
-    prototypalStackArr.push(PrototypalStack());
-    prototypalQueueArr.push(PrototypalQueue());
-    pseudoclassicStackArr.push(new PseudoClassicalStack());
-    pseudoclassicQueueArr.push(new PseudoClassicalQueue());
+    // funcShareStackArr.push(FunctionalSharedStack());
+    // funcShareQueueArr.push(FunctionalSharedQueue());
+    // prototypalStackArr.push(PrototypalStack());
+    // prototypalQueueArr.push(PrototypalQueue());
+    // pseudoclassicStackArr.push(new PseudoClassicalStack());
+    // pseudoclassicQueueArr.push(new PseudoClassicalQueue());
 
     if (i === stackAndQueueSizes) {
       $('.frame').text('We\'re done!');
@@ -27,3 +27,11 @@ $(document).ready(function() {
     }
   }
 });
+
+/*
+functional pattern:
+functional shared pattern:
+prototypal pattern:
+pseudoclassical pattern:
+
+*/
