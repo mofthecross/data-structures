7013c1b79bbed6aa00df88a3d35978b407e3a685
mofthecross <mofthecross@gmail.com>
2016-06-10 19:47:17 -0700

    Begin to implement DoubleLinkedList's addToTail

--- advanced/src/doubleLinkedList.js
+++ advanced/src/doubleLinkedList.js
@@ -1,14 +1,21 @@
 var DoubleLinkedList = function() {
+  this.list = {};
   this.head = null;
   this.tail = null;
 };
 
-DoubleLinkedList.prototype.addToHead = function() {
+DoubleLinkedList.prototype.addToHead = function(value) {
   
 };
 
-DoubleLinkedList.prototype.addToTail = function() {
-
+DoubleLinkedList.prototype.addToTail = function(value) {
+  this.list[value] = Node(value);
+  if (this.head === null) {
+    this.head === this.list[value];
+    this.tail === this.list[value];
+  } else {
+    this.tail === this.
+  }
 };
 
 DoubleLinkedList.prototype.contains = function(target) {
@@ -29,4 +36,14 @@ DoubleLinkedList.prototype.listForwards = function() {
 
 DoubleLinkedList.prototype.listBackwards = function() {
   // body...
+};
+
+DoubleLinkedList.prototype.Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.prev = null;
+
+  return node;
 };
